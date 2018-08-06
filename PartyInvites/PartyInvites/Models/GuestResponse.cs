36b27362ce31fcb24800abdc1db4b6d1ee538115
wangjj <15708455260@163.com>
2018-08-06 20:46:59 +0800

    添加第一个mvcDemo

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public Boolean WillAttend { get; set; }
+    }
+}
